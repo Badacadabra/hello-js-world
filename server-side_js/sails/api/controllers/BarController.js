8b5b78dda334a825776efee81a61869e8cc69e9b
Badacadabra <baptiste.vannesson@gmail.com>
2017-04-07 03:48:51 +0200

    Add Sails

---
+++ server-side_js/sails/api/controllers/BarController.js
@@ -0,0 +1,13 @@
+/**
+ * BarController
+ *
+ * @description :: Server-side logic for managing bars
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  'hello': function (req, res) {
+    res.send('Hello, Bar!');
+  }
+};
+

59908597c3b266ec1c241efa3bb56c877dc9a6d5
Badacadabra <baptiste.vannesson@gmail.com>
2017-04-26 22:54:50 +0200

    Add last exercise of 'count-to-6'

--- nodeschool/count-to-6/tagged_template_strings.js
+++ nodeschool/count-to-6/tagged_template_strings.js
@@ -1,1 +1,19 @@
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html (strs, ...args) {
+  let p1 = strs[0],
+      p2 = args[0],
+      p3 = strs[1],
+      p4 = args[1],
+      p5 = strs[2];
+
+  return `${p1}${entities(p2)}${p3}${entities(p4)}${p5}`;
+}
+
+function entities(str) {
+  return str.replace(/&/g, '&amp;')
+            .replace(/>/g, '&gt;')
+            .replace(/</g, '&lt')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&apos;');
+}

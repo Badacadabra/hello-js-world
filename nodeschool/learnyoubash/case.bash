16403306b2e0870b31efe49b3421807147f8999f
Badacadabra <baptiste.vannesson@gmail.com>
2017-05-09 23:43:51 +0200

    Add new exercises from 'learnyoubash'

---
+++ nodeschool/learnyoubash/case.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case "$1" in
+  jpeg|jpg)
+    echo "It is jpeg."
+    ;;
+  png)
+    echo "It is png."
+    ;;
+  gif)
+    echo "It is gif."
+    ;;
+  *)
+    echo "$1 is not an image!"
+    ;;
+esac

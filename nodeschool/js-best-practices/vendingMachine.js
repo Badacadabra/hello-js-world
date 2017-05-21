7a20072d75c68bc7b7e4a830424e1a54b942e8a3
Badacadabra <baptiste.vannesson@gmail.com>
2017-05-21 23:35:46 +0200

    Add 'js-best-practices'

---
+++ nodeschool/js-best-practices/vendingMachine.js
@@ -0,0 +1,32 @@
+var balanceManager = require('./balanceManager');
+var changeHandler = require('./changeHandler');
+var productInventory = require('./productInventory');
+
+module.exports = {
+
+  vendProduct: function(productId){
+    var product = productInventory.getProduct(productId);
+    balanceManager.decreaseBalance(product.price);
+    return product;
+  }
+
+  releaseChange: function(){
+    var currentBalance = balanceManager.getBalance();
+    balanceManager.decreaseBalance(currentBalance);
+    return this.convertToChange(currentBalance);
+  },
+
+  insertCoin: function(coinType){
+    var value = changeHandler.getAmount(coinType);
+    balanceManager.increaseBalance(value);
+  },
+
+  isValidAmount: function(amount){
+    if(amount === null){
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+};
